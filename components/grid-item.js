5ea9277d4930e9643cd8997bef9108f5ae35cbbd
SunjidmaaL <svnjee@gmail.com>
2021-05-15 13:05:56 +0800

    unsplash тэй холбосон

--- components/grid-item.js
+++ components/grid-item.js
@@ -1,44 +1,44 @@
 import { Card } from "react-bootstrap";
 import Link from "next/link";
+import Image from "next/image";
+import moment from "moment";
+import { urlFor } from "lib/api";
 
-export default ({ post} ) => {
-    return (
-        <Card className={`fj-card`}>
-                <div className="card-body-wrapper">
-                  <Card.Header className="d-flex flex-row">
-                    <img
-                      src={post.publisher.picture}
-                      className="rounded-circle mr-3"
-                      height="50px"
-                      width="50px"
-                      alt="avatar"
-                    />
-                    <div>
+export default ({ post }) => {
 
-                      <Card.Title className="font-weight-bold mb-1">
-                        {post.publisher.title}
-                      </Card.Title>
-                      <Card.Text className="card-date">
-                        {post.date}
-                      </Card.Text>
-                    </div>
-                  </Card.Header>
+  moment.locale('mn');
 
-                  <Link href={'/${post.slug}'}>
-                  <a>
-                  <div className="view overlay">
-                    <Card.Img
-                      src={post.image} alt="Card image cap"
-                    />
-                  </div>
+  return (
+    <Card className={`fj-card`}>
+      <div className="card-body-wrapper">
+        <Card.Header className="d-flex flex-row">
+          <img
+            src={urlFor(post.publisher.picture).height(50).url()}
+            className="rounded-circle mr-3"
+            height="50px"
+            width="50px"
+          />
+          <div>
+            <Card.Title className="font-weight-bold mb-1">
+              {post.publisher.title}
+            </Card.Title>
+            <Card.Text className="card-date">{moment(post.date).format("MMM Do YY")
+            }</Card.Text>
+          </div>
+        </Card.Header>
 
-                  <Card.Body>
-                    <Card.Title className="card-main-title">{post.title}</Card.Title>
-                    <Card.Text>{post.subtitle}</Card.Text>
-                  </Card.Body>
-                  </a>
-                  </Link>
-                </div>
-              </Card>
-    )
-}
+        <Link href={`/${post.slug}`}>
+          <a>
+            <div className="view overlay">
+              <Card.Img src={urlFor(post.image).height(300).url()} alt="Card image cap" />
+            </div>
+            <Card.Body>
+              <Card.Title className="card-main-title">{post.title}</Card.Title>
+              <Card.Text>{post.subtitle}</Card.Text>
+            </Card.Body>
+          </a>
+        </Link>
+      </div>
+    </Card>
+  );
+};
